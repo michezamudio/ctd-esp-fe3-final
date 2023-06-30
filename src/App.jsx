0404aeb380db0d1fc0e5f4c984a616ec80d4ad22
michell <michellguadalupezamudioizasa@gmail.com>
2023-06-30 00:20:00 -0500

    actualizacion

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+
+function App() {
+  return (
+      <div className= "App">
+          <Navbar/>
+          <h1>Hola Mundo</h1>
+          <Footer/>
+      </div>
+  );
+}
+
+export default App;
